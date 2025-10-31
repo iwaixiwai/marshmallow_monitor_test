c698b7a7bd7984f610173312e1430f66a9750df9
Toshio Iwai <iwai@gol.com>
2025-10-31 12:56:22 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-// ウェブカメラの映像を表示
+// マシュマロモニターを再現する
+// by Toshio Iwai 2025
 
-let img;
-let img2 = []; //画像の配列をつくる
+let img;  // マシュマロ写真用
+let img2 = []; //連続キャプチャー用の画像配列をつくる
 
-function preload() {
-img = loadImage("m_monitor.png"); 
+// 写真を読み込む
+function preload() 
+{
+  img = loadImage("m_monitor.png"); 
 }
 
+// カメラ設定用
 const facingModeList = [
   "user",
   "environment",
@@ -39,6 +43,7 @@ function setup()
 {
   angleMode(DEGREES);
   
+  // 画面の最大サイズを制限
   if(windowWidth < 1301)
   {
     createCanvas(windowWidth, windowHeight);
@@ -47,22 +52,25 @@ function setup()
   {
     createCanvas(1301,1883);      
   }
-      capture = createCapture(VIDEO);
-      capture.hide();
   
+  capture = createCapture(VIDEO);
+  capture.hide();
+
+  // キャプチャーする画像は4:3のサイズ
   w = capture.width;
-//  h = capture.height;
   h = w*3/4;
-  
+
+  // マシュマロモニター写真に合わせるためのパラメータ
   image_scale = width / 1301;
   mx = image_scale * 645;
-  my = image_scale * 618;
+  my = image_scale * 620;
   ww = (int)(image_scale * 296);
   hh = (int)(image_scale * 220);
-
   video_scale = hh / 120;
-    
-  for (mode of facingModeList) {
+
+  // カメラ設定
+  for (mode of facingModeList) 
+  {
     options.push({
       video: {
         facingMode: mode,
@@ -71,35 +79,39 @@ function setup()
     });
   }
 
+  // キャプチャー用画像配列を用意
   for(i=0;i<120;i++)
-    {
-      img2[i] = capture.get(0, 0, w, h);
-    }
+  {
+    img2[i] = capture.get(0, 0, w, h);
+  }
 }
 
 function draw() 
 {
   background(0);
 
+  // キャプチャーする配列番号を更新
   t++;
   if(t>119)
   {
     t = 0;
   }
     
+  // ビデオ画像の中央部分を配列にキャプチャー
   w = capture.width;
-//  h = capture.height;
   h = w*3/4;
-  
   img2[t] = capture.get(0, (capture.height - h)/2, w, h);
   
   push();
+  
+  // 左右反転
   translate(mx+ww,my+hh);
   rotate(180);
+
+  // 過去のビデオ画像から、表示用の画像を再構成する
   for(i = 0; i<120 ; i+=1)
   {
-    frame_no = (t+i)%120;
-//    image(img2[(t+i)%218], 0, i, ww, 1, 0, h-i, w, 1);
+    frame_no = (t+i)%120; // 時間をずらす
     image(img2[frame_no], 0, i*video_scale, ww, 2, 0, h-(int)(i*(h/120)), w, 2);
   }
   
@@ -113,7 +125,7 @@ function draw()
 
   pop();
 
-  // マシュマロ画像合成
+  // マシュマロ写真合成
   image(img, 0, 0, width, width*img.height/img.width);
   
 }
