d35cc74e6428e7ed70038b168ff212a4597e811b
iwaixiwai <iwai@gol.com>
2025-10-31 11:58:16 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -51,7 +51,8 @@ function setup()
       capture.hide();
   
   w = capture.width;
-  h = capture.height;
+//  h = capture.height;
+  h = w*3/4;
   
   image_scale = width / 1301;
   mx = image_scale * 645;
@@ -89,7 +90,7 @@ function draw()
   w = capture.width;
   h = capture.height;
   img2[t] = capture.get(0, 0, w, h);
-
+  
   push();
   translate(mx+ww,my+hh);
   rotate(180);
@@ -101,7 +102,7 @@ function draw()
   }
   
   // モニター部分に走査線を重ねる
-  stroke(255,255,255,60);
+  stroke(100,170,240,200);
   for(i = 0; i<hh ; i+=2)
   {
     line(0,i,ww,i);
